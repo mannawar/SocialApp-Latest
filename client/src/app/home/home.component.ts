96ac941e31a96472bfdc70a0c96a8335a2846fb5
mannawar <dearmannawar@gmail.com>
2023-01-15 13:42:27 +0300

    End of section 9

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -12,20 +12,12 @@ export class HomeComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.getUsers();
+
   }
 
   registerToggle()
   {
     this.registerMode = !this.registerMode;
-  }
-
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log("Request has been completed")
-    })
   }
 
   cancelRegisterMode(event: boolean){
