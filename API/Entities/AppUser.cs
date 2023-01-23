4ce5646d1e6e6d463ab824a29fd4fc830b52d238
mannawar <dearmannawar@gmail.com>
2023-01-23 19:32:37 +0300

    End of section 15

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -24,9 +24,7 @@ namespace API.Entities
         public List<UserLike> LikedByUsers { get; set; }
         public List<UserLike> LikedUsers { get; set; }
 
-        // public int GetAge()
-        // {
-        //     return DateOfBirth.CalculateAge();
-        // }
+        public List<Message> MessageSent { get; set; }
+        public List<Message> MessageReceived { get; set; }
     }
 }
--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -15,7 +15,9 @@ namespace API.Helpers
             CreateMap<Photo, PhotoDto>();
             CreateMap<MemberUpdateDto, AppUser>();
             CreateMap<RegisterDto, AppUser>();
-
+            CreateMap<Message, MessageDto>()
+                .ForMember(d => d.SenderPhotoUrl, o => o.MapFrom(s => s.Sender.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(d => d.RecipientPhotoUrl, o =>o.MapFrom(r => r.Recipient.Photos.FirstOrDefault(x => x.IsMain).Url));
         }
     }
 }
